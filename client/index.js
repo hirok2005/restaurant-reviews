59845e8df926a40d794a45334e2d7f21ee67225e
hirok2005 <hirok2005@gmail.com>
2024-01-12 15:36:50 +0000

    added events section

--- client/index.js
+++ client/index.js
@@ -1,3 +1,64 @@
+let searchType = 1; // 1 for restaurants 0 for events
+const searchHtml = `<h1 style="text-align:center" id="searchTypeName">Restaurants</h1>
+<form id="search-form">
+  <div class="input-group mx-auto w-75">
+    <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+      aria-expanded="false">Filters</button>
+    <ul class="dropdown-menu">
+      <li>
+        <div class="p-2" id="ratings">
+          <h5>Rating</h5>
+          <div class="form-check form-check-inline">
+            <label class="form-check-label" for="0">
+              <input class="form-check-input" type="radio" name="rating" id="0" value="0" checked="checked">
+              0 and up
+            </label>
+          </div>
+          <div class="form-check form-check-inline">
+            <label class="form-check-label" for="1">
+              <input class="form-check-input" type="radio" name="rating" id="1" value="1">
+              1 and up
+            </label>
+          </div>
+          <div class="form-check form-check-inline">
+            <label class="form-check-label" for="2">
+              <input class="form-check-input" type="radio" name="rating" id="2" value="2">
+              2 and up
+            </label>
+          </div>
+          <div class="form-check form-check-inline">
+            <label class="form-check-label" for="3">
+              <input class="form-check-input" type="radio" name="rating" id="3" value="3">
+              3 and up
+            </label>
+          </div>
+          <div class="form-check form-check-inline">
+            <label class="form-check-label" for="4">
+              <input class="form-check-input" type="radio" name="rating" id="4" value="4">
+              4 and up
+            </label>
+          </div>
+          <div class="form-check form-check-inline">
+            <label class="form-check-label" for="5">
+              <input class="form-check-input" type="radio" name="rating" id="5" value="5">
+              5 and up
+            </label>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="p-2"><input class="form-control w-auto btn-sm btn-md-none" id="city" name="city" type="text"
+            placeholder="City" /></div>
+      </li>
+    </ul>
+    <input id="name" name="name" type="text" class="form-control" aria-label="Text input with dropdown button" />
+    <button class="btn btn-primary btn-sm btn-md-none" type="submit">Search</button>
+  </div>
+</form>
+<div class="container container-sm container-md-none">
+  <div id="results" class="row row-cols-sm-2 row-cols-md-3 p-5 mb-2"></div>
+</div>
+</div>`;
 const restaurantButton = document.getElementById('restaurant');
 // TODO make error pages nice
 
@@ -22,71 +83,38 @@ function isOpen (opening) {
     return !(times[1] <= currTime && currTime < times[0]);
 }
 
-async function loadSearchRestaurants () {
+async function loadSearchDiv () {
     const content = document.getElementById('main-content');
-    const html = `<h1 style="text-align:center">Restaurants</h1>
-        <form id="search-restaurants">
-        <div class="input-group mx-auto w-75">
-        <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Filters</button>
-        <ul class="dropdown-menu">
-            <li>
-                <div class="p-2">
-                        <h5>Rating</h5>
-                        <div class="form-check form-check-inline">
-                            <label class="form-check-label" for="0">    
-                                <input class="form-check-input" type="radio" name="rating" id="0" value="0" checked="checked">
-                                0 and up
-                            </label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <label class="form-check-label" for="1">
-                                <input class="form-check-input" type="radio" name="rating" id="1" value="1">
-                                1 and up
-                            </label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <label class="form-check-label" for="2">    
-                                <input class="form-check-input" type="radio" name="rating" id="2" value="2">
-                                2 and up
-                            </label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <label class="form-check-label" for="3">
-                                <input class="form-check-input" type="radio" name="rating" id="3" value="3">
-                                3 and up
-                            </label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <label class="form-check-label" for="4">
-                                <input class="form-check-input" type="radio" name="rating" id="4" value="4">
-                                4 and up
-                            </label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <label class="form-check-label" for="5">        
-                                <input class="form-check-input" type="radio" name="rating" id="5" value="5">
-                                5 and up
-                            </label>
-                        </div>
-                </div>
-            </li>
-            <li><div class="p-2"><input class="form-control w-auto btn-sm btn-md-none" id="city" name="city" type="text" placeholder="City"/></div></li>
-        </ul>
-        <input id="name" name="name" type="text" class="form-control" aria-label="Text input with dropdown button"/>
-        <button class="btn btn-primary btn-sm btn-md-none" type="submit">Search</button>
+    content.innerHTML = searchHtml;
+    const resultDiv = document.getElementById('results');
+    let html = '';
+    for (let i = 0; i < 9; i++) {
+        html += `<div><div class="card mb-3" aria-hidden="true">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title placeholder-glow">
+            <span class="placeholder col-6"></span>
+          </h5>
+          <p class="card-text placeholder-glow">
+            <span class="placeholder col-7"></span>
+            <span class="placeholder col-4"></span>
+            <span class="placeholder col-4"></span>
+            <span class="placeholder col-6"></span>
+            <span class="placeholder col-8"></span>
+          </p>
+          <a class="btn btn-primary disabled placeholder col-6" aria-disabled="true"></a>
         </div>
-        </form>
-        <div id="results" class="d-flex align-items-center justify-content-center"></div>
-        `;
-    content.innerHTML = html;
-    await searchRestaurants('0');
-    const submitButton = document.getElementById('search-restaurants');
-    submitButton.addEventListener('submit', async function (event) {
+      </div></div>`;
+    }
+    resultDiv.innerHTML = html;
+    await search('0');
+    const submitForm = document.getElementById('search-form');
+    submitForm.addEventListener('submit', async function (event) {
         event.preventDefault();
         const rating = document.querySelector('input[name="rating"]:checked').value;
         const name = document.getElementById('name').value;
         const city = document.getElementById('city').value;
-        await searchRestaurants(rating, city, name);
+        await search(rating, city, name);
     });
 }
 
@@ -94,7 +122,8 @@ async function loadRestaurant (event) {
     event.preventDefault();
     const content = document.getElementById('main-content');
     try {
-        const response = await fetch('http://192.168.0.12:8090/restaurant/?id=' + event.target.id, { signal: AbortSignal.timeout(5000) });
+        console.log(event.target.id);
+        const response = await fetch('http://127.0.0.1:8090/restaurant/?ID=' + event.target.id, { signal: AbortSignal.timeout(5000) });
         console.log(response.type);
         if (!isOk(response)) {
             return;
@@ -105,36 +134,47 @@ async function loadRestaurant (event) {
         <p class="p-5 mb-2">${info.description}</p>
         `;
     } catch (e) {
-        console.log(e, e.name);
-        if (e.name === '') {
-            const mod = new bootstrap.Modal(document.getElementById('exampleModal'));
-            mod.show();
-            return;
-        }
         content.innerHTML = `<div class="alert alert-danger" role="alert">
         Something wrong happened please try again ${e}</div>`;
     }
 }
 
-async function searchRestaurants (rating = '', city = '', name = '') {
+async function search (rating = '', city = '', name = '') {
+    let response;
     const content = document.getElementById('main-content');
-    const query = `rating=${rating}&city=${city}&name=${name}`;
+    const query = searchType === 1 ? `rating=${rating}&city=${city}&name=${name}` : `city=${city}&name=${name}`;
     try {
-        const response = await fetch('http://192.168.0.12:8090/restaurants?' + query);
+        console.log(searchType);
+        if (searchType === 1) {
+            response = await fetch('http://127.0.0.1:8090/restaurants?' + query);
+        } else {
+            response = await fetch('http://127.0.0.1:8090/events?' + query);
+        }
         if (!isOk(response)) {
             return;
         }
-        const restaurants = await response.json();
+        const data = await response.json();
         const results = document.getElementById('results');
-        let html = '<div class="container container-sm container-md-none">';
-        html += '<div class="row row-cols-sm-2 row-cols-md-3 p-5 mb-2">';
-        for (let i = 0; i < restaurants.length; i++) {
-            html += `<div class="col mb-5 p-4">
-                        <a class="text-reset text-decoration-none" href=# onclick="loadRestaurant(event)"><h5 id="${restaurants[i].id}">${restaurants[i].name}</h5><a>
+        if (data.length === 0) {
+            results.innerHTML = '<h1 class="alert alert-danger p-3 mb-2" style="margin-top: 100px;">No results found!<h1>';
+            return;
+        }
+        let html = '';
+        for (let i = 0; i < data.length; i++) {
+            let title = data[i].name.substring(0, 17);
+            if (data[i].name.length > 17) {
+                title += '...';
+            }
+            html += `<div><div class="card col mb-3">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                        <h5 class="card-title text-reset text-decoration-none text-nowrap" href=#>${title}</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary" onclick=${searchType === 1 ? 'loadRestaurant(event)' : 'loadEvent(event)'} id="${data[i].ID}">Info Page</a>
+                        </div>
+                    </div>
                     </div>`;
         }
-        html += '</div>';
-        html += '</div>';
         results.innerHTML = html;
     } catch (e) {
         content.innerHTML = `<div class="alert alert-danger" role="alert">
@@ -142,17 +182,23 @@ async function searchRestaurants (rating = '', city = '', name = '') {
     }
 }
 
+function changeType (type) {
+    searchType = type;
+    loadSearchDiv();
+    if (type === 1) {
+        document.getElementById('searchTypeName').innerHTML = 'Restaurants';
+        document.getElementById('ratings').removeAttribute('hidden');
+        return;
+    }
+    document.getElementById('searchTypeName').innerHTML = 'Events';
+    document.getElementById('ratings').setAttribute('hidden', 'hidden');
+}
+
 restaurantButton.addEventListener('click', async function (event) {
     event.preventDefault();
-    loadSearchRestaurants();
+    loadSearchDiv();
 });
 
 document.addEventListener('DOMContentLoaded', async function (event) {
-    loadSearchRestaurants();
-});
-
-document.addEventListener('online', function () {
-    if (navigator.onLine) {
-
-    }
+    loadSearchDiv();
 });
--- server.js
+++ server.js
@@ -1,24 +1,25 @@
+const path = require('node:path');
 const Fuse = require('fuse.js');
 const express = require('express');
 const fs = require('fs');
 const app = express();
 
-const filterName = { keys: ['name', 'style'] };
-
-const data = JSON.parse(fs.readFileSync('./data.json'));
+const data = JSON.parse(fs.readFileSync(path.join(__dirname, 'data.json')));
 
 const restaurants = data.restaurants;
 const reviews = data.reviews;
 const events = data.events;
+const restaurantSearch = new Fuse(restaurants, { keys: ['name', 'style'] });
+const reviewSearch = new Fuse(reviews, { keys: ['restaurantID'] });
+const eventSearch = new Fuse(events, { keys: ['eventID'] });
 
 app.use(express.static('client'));
 
 app.get('/restaurants/', function (request, response) {
-    console.log(request);
+    response.setHeader('Content-Type', 'application/json');
     let filterRestaurants = restaurants;
     if (request.query.name !== undefined && request.query.name !== '') {
-        const f = new Fuse(filterRestaurants, filterName);
-        filterRestaurants = f.search(request.query.name).map(item => item.item);
+        filterRestaurants = restaurantSearch.search(request.query.name).map(item => item.item);
     }
     if (request.query.city !== undefined && request.query.city !== '') {
         filterRestaurants = filterRestaurants.filter(restaurant => restaurant.address[1].toLowerCase() === request.query.city.toLowerCase().trim());
@@ -26,12 +27,19 @@ app.get('/restaurants/', function (request, response) {
     if (request.query.rating !== undefined) {
         filterRestaurants = filterRestaurants.filter(restaurant => restaurant.rating >= parseInt(request.query.rating));
     }
-    response.json(filterRestaurants.map((restaurant) => ({ id: restaurant.id, name: restaurant.name })));
+    response.json(filterRestaurants.map((restaurant) => ({ ID: restaurant.ID, name: restaurant.name })));
 });
 
 app.get('/restaurant/', function (request, response) {
+    // return all data of restaurant if no other query other than ID
+    response.setHeader('Content-Type', 'application/json');
+    if (request.query.ID === undefined) {
+        response.sendStatus(404);
+        return;
+    }
     for (const restaurant of restaurants) {
-        if (request.query.id !== undefined && request.query.id === restaurant.id) {
+        if (request.query.ID === restaurant.ID) {
+            // if request.query
             response.json(restaurant);
             return;
         }
@@ -39,9 +47,73 @@ app.get('/restaurant/', function (request, response) {
     response.sendStatus(404);
 });
 
-app.get('/favicon.ico', function (request, response) {
-    response.sendFile('./client/favicon.ico');
+app.get('/reviews/', function (request, response) {
+    response.setHeader('Content-Type', 'application/json');
+    let filterReviews = reviews;
+    console.log(filterReviews);
+    if (request.query.restaurantID !== undefined && request.query.restaurantID !== '') {
+        filterReviews = reviewSearch.search(request.query.restaurantID).map(item => item.item);
+    }
+    if (request.query.rating !== undefined) {
+        filterReviews = filterReviews.filter(review => review.rating >= parseInt(request.query.rating));
+    }
+    console.log(filterReviews);
+    response.json(filterReviews.map((review) => ({ ID: review.ID, name: review.name })));
 });
+
+app.get('/review/', function (request, response) {
+    // return all data of restaurant if no other query other than ID
+    response.setHeader('Content-Type', 'application/json');
+    if (request.query.ID === undefined) {
+        response.sendStatus(404);
+        return;
+    }
+    for (const review of reviews) {
+        if (request.query.ID === review.ID) {
+            // if request.query
+            response.json(review);
+            return;
+        }
+    }
+    response.sendStatus(404);
+});
+
+app.get('/events/', function (request, response) {
+    response.setHeader('Content-Type', 'application/json');
+    response.json([]);
+    // return;
+    // let filterEvents = events;
+    // // console.log(filterReviews);
+    // if (request.query.eventID !== undefined && request.query.eventID !== '') {
+    //     filterEvents = eventSearch.search(request.query.eventID).map(item => item.item);
+    // }
+    // if (request.query.city !== undefined && request.query.city !== '') {
+    //     filterEvents = filterEvents.filter(event => event.address[1].toLowerCase() === request.query.city.toLowerCase().trim());
+    // }
+    // // console.log(filterReviews);
+    // response.json(filterEvents.map((event) => ({ ID: event.ID, name: event.name })));
+});
+
+app.get('/event/', function (request, response) {
+    // return all data of restaurant if no other query other than ID
+    response.setHeader('Content-Type', 'application/json');
+    if (request.query.ID === undefined) {
+        response.sendStatus(404);
+        return;
+    }
+    for (const event of events) {
+        if (request.query.ID === event.ID) {
+            // if request.query
+            response.json(event);
+            return;
+        }
+    }
+    response.sendStatus(404);
+});
+
+// app.get('/img/', function (request, response) {
+//     response.setHeaderx
+// })
 
 app.use(function (request, response) {
     if (request.accepts('html')) {
